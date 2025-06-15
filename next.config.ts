78825a0e0684a20f7fe9a903ef88260440f4f554
duxizhi <duxizhi@ones.ai>
2025-06-15 21:18:18 +0800

    chore: nextjs 注入 env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    HOST: process.env.HOST || "localhost",
+    CONFIG_VALUE: process.env.CONFIG_VALUE || "Default Value",
+  },
 };
 
 export default nextConfig;
